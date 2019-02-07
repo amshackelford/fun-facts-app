7bb431a0434365bba0831bc9734e2aff6afe5a86
Anthony Shackelford <anthony.shackelford@hotmail.com>
2019-02-07 02:43:58 -0500

    Initial commit

---
+++ app/src/main/java/com/shackman/funfacts/ColorWheel.java
@@ -0,0 +1,33 @@
+package com.shackman.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+public class ColorWheel {
+    // Fields or Member Variables - Properties about the object
+    private String[] colors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    // Methods = Actions the object can take
+    int getColor() {
+        // Randomly select a color
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(colors.length);
+        int color = Color.parseColor(colors[randomNumber]);
+        return color;
+    }
+}
---
+++ app/src/main/java/com/shackman/funfacts/FunFactsActivity.java
@@ -0,0 +1,48 @@
+package com.shackman.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    // Declare our View variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assign the views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = factBook.getFact();
+
+                // Update the screen with our new fact
+                factTextView.setText(fact);
+
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        Log.d(TAG, "We're logging from the onCreate() method!");
+    }
+}
