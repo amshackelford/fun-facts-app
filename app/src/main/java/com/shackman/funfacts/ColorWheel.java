658191c33132f2ab810698b01ae949843dd2c274
Anthony Shackelford <anthony.shackelford@hotmail.com>
2019-02-07 10:27:12 -0500

    Add bronze color

--- app/src/main/java/com/shackman/funfacts/ColorWheel.java
+++ app/src/main/java/com/shackman/funfacts/ColorWheel.java
@@ -20,7 +20,8 @@ public class ColorWheel {
             "#e0ab18", // mustard
             "#637a91", // dark gray
             "#f092b0", // pink
-            "#b7c0c7"  // light gray
+            "#b7c0c7", // light gray
+            "#8b4513"  // bronze
     };
 
     // Methods = Actions the object can take
